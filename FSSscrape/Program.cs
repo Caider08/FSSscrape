5e036d5868f292ae05fd4ac6ff558af771c99d57
Caider08 <caidbuch@ksu.edu>
2017-12-03 19:29:26 -0600

    initial push

--- FSSscrape/Program.cs
+++ FSSscrape/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
 
 namespace FSSscrape
 {
@@ -10,6 +14,41 @@ namespace FSSscrape
     {
         static void Main(string[] args)
         {
+            string test = "";
+
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://www.fansharesports.com/shared/signIn?redirectUrl=%2Fgolf%2Ftrends");
+
+                var userName = driver.FindElementById("email");
+                var passWord = driver.FindElementById("password");
+                var logIn = driver.FindElementById("submit");
+
+                userName.SendKeys("caiderwaider@hotmail.com");
+                passWord.SendKeys("DKscrape");
+                logIn.Click();
+
+                System.Threading.Thread.Sleep(25);
+
+                //var contentWrap = driver.FindElementById("content-wrap");
+
+                driver.Navigate().GoToUrl("https://www.fansharesports.com/golf/analytics/tags");
+
+                
+
+                var main = driver.FindElementById("main");
+
+                test = main.Text;
+
+                
+
+                
+
+            }
+
+            Console.WriteLine(test);
+            Console.ReadLine();
+            
         }
     }
 }
